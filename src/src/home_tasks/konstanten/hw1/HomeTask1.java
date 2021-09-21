4ffea4c412306caa4155c91afc82ac44ea21ac16
Konstanten Nekhaychuk <knekhaychuk@credible.com>
2021-09-21 19:27:16 +0300

    HomeTask1 KN

--- src/src/home_tasks/konstanten/hw1/HomeTask1.java
+++ src/src/home_tasks/konstanten/hw1/HomeTask1.java
@@ -2,7 +2,23 @@ package home_tasks.konstanten.hw1;
 
 public class HomeTask1 {
     public static void main(String[] args) {
+        byte by1 = 4;
+        boolean t = true;
+        short sh = 100;
+        double double1 = 200;
+        int int1 = 100;
+        long lng = 66;
+        char chr = 500;
+        float flt = 5000;
+
         System.out.println("Hello World!");
+        System.out.println(double1);
+        System.out.println(int1);
+        System.out.println(by1);
+        System.out.println(t);
+        System.out.println(sh);
+        System.out.println(lng);
+        System.out.println(chr);
+        System.out.println(flt);
     }
 }
-fff
