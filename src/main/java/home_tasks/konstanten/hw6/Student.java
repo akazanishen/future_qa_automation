d3f3348b59009559b7dd557cbadab04086e1f104
Konstanten Nekhaychuk <knekhaychuk@credible.com>
2021-12-07 14:49:17 +0200

    HomeTask7 KN

---
+++ src/main/java/home_tasks/konstanten/hw6/Student.java
@@ -0,0 +1,21 @@
+package home_tasks.konstanten.hw6;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+
+    int course;
+    String name;
+    String group;
+    int grades;
+
+    Student(int course, String name, String group, int grades){
+        this.course = course;
+        this.name = name;
+        this.group = group;
+        this.grades = grades;
+    }
+
+}
+
