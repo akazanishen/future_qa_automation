3b05c7c6545ce8865d8395bceaabd41ab44bcc34
dtrehubova <dtrehubova@credible.com>
2021-12-13 21:53:08 +0200

    Diana HW8 - applied review comments

--- src/main/java/home_tasks/diana/hw8/Task1and2.java
+++ src/main/java/home_tasks/diana/hw8/Task1and2.java
@@ -5,6 +5,8 @@ import home_tasks.diana.hw6.pages.HomePage;
 import home_tasks.diana.hw6.Config;
 import home_tasks.diana.hw6.pages.ModalWindow;
 import org.testng.annotations.Test;
+import java.util.Arrays;
+import java.util.List;
 
 public class Task1and2 extends Config {
 
@@ -33,7 +35,9 @@ public class Task1and2 extends Config {
         modalWindow.enterPasswordConf();
         modalWindow.clickCreateBtn();
         homePage.verifyUserSignedUp();
-        homePage.verifyDropDownOptions();
+        List<String> expectedDropdownOptions = Arrays.asList(
+                "Messages", "Details", "Orders list", "Log out");
+        homePage.verifyDropDownOptions(expectedDropdownOptions);
     }
 
 }
